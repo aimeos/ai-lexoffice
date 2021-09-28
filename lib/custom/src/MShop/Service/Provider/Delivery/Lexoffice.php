d6562053fed4cdff964469d2cacc09dc92585e22
Aimeos <aimeos@aimeos.org>
2021-09-28 17:40:16 +0200

    First basic implementation

---
+++ lib/custom/src/MShop/Service/Provider/Delivery/Lexoffice.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2021
+ * @package MShop
+ * @subpackage Service
+ */
+
+
+namespace Aimeos\MShop\Service\Provider\Delivery;
+
+use \Aimeos\MW\Logger\Base as Log;
+
+
+/**
+ * Lexoffice delivery provider implementation
+ *
+ * @package MShop
+ * @subpackage Service
+ */
+class Lexoffice
+	extends \Aimeos\MShop\Service\Provider\Delivery\Base
+	implements \Aimeos\MShop\Service\Provider\Delivery\Iface
+{
+	private $beconfig = [
+		'lexoffice.apikey' => [
+			'code' => 'lexoffice.apikey',
+			'internalcode'=> 'lexoffice.apikey',
+			'label'=> 'Lexoffice API key',
+			'type'=> 'string',
+			'internaltype'=> 'string',
+			'default'=> '',
+			'required'=> true,
+		],
+	];
+
+
+	/**
+	 * Checks the backend configuration attributes for validity
+	 *
+	 * @param array $attributes Attributes added by the shop owner in the administraton interface
+	 * @return array An array with the attribute keys as key and an error message as values
+	 */
+	public function checkConfigBE( array $attributes ) : array
+	{
+		$errors = parent::checkConfigBE( $attributes );
+		return array_merge( $errors, $this->checkConfig( $this->beconfig, $attributes ) );
+	}
+
+
+	/**
+	 * Returns the configuration attribute definitions
+	 *
+	 * @return \Aimeos\MW\Common\Critera\Attribute\Iface[] List of attribute definitions
+	 */
+	public function getConfigBE() : array
+	{
+		$list = parent::getConfigBE();
+
+		foreach( $this->beconfig as $key => $config ) {
+			$list[$key] = new \Aimeos\MW\Criteria\Attribute\Standard( $config );
+		}
+
+		return $list;
+	}
+
+
+	/**
+	 * Sends the order to the Lexoffice API
+	 *
+	 * @param \Aimeos\MShop\Order\Item\Iface $order Order item
+	 * @return \Aimeos\MShop\Order\Item\Iface Modified order item
+	 */
+	public function process( \Aimeos\MShop\Order\Item\Iface $order ) : \Aimeos\MShop\Order\Item\Iface
+	{
+		$parts = \Aimeos\MShop\Order\Item\Base\Base::PARTS_ALL;
+		$basket = $this->getOrderBase( $order->getBaseId(), $parts );
+
+		// send the order details to an external system
+
+		return $order->setDeliveryStatus( \Aimeos\MShop\Order\Item\Base::STAT_PROGRESS );
+	}
+
+
+	/**
+	 * Sends a request to the Mangopay server
+	 *
+	 * @param string $path Relative path of the resource (without client ID)
+	 * @param array $body Associative list of data to send
+	 * @param string $method Request method (GET, PATCH, POST, PUT, DELETE)
+	 * @return array Array of returned data and HTTP status code
+	 */
+	protected function send( string $path, array $body = [], string $method = 'GET' ) : array
+	{
+		if( ( $ch = curl_init() ) === false ) {
+			throw new \RuntimeException( 'Initializing CURL connection failed' );
+		}
+
+		$url = 'https://api.lexoffice.io/' . $path;
+		$body = !empty( $body ) ? json_encode( $body ) : '';
+
+		$header = [
+			'Authorization: Bearer ' . $this->getConfigValue( 'lexoffice.apikey' ),
+			'Content-Length: ' . strlen( $body ),
+			'Content-Type: application/json',
+			'Accept: application/json',
+			'Cache-control: no-cache'
+		];
+
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, $header );
+		curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, $method );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5 );
+		curl_setopt( $ch, CURLOPT_TIMEOUT, 5 );
+
+		if( in_array( $method, ['PATCH', 'POST', 'PUT'] ) )
+		{
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $body );
+			curl_setopt( $ch, CURLOPT_POST, true );
+		}
+
+		if( ( $response = curl_exec( $ch ) ) === false ) {
+			throw new \RuntimeException( sprintf( 'Curl exec failed for "%1$s": %2$s', $url, curl_error( $ch ) ) );
+		}
+
+		if( ( $errno = curl_errno( $ch ) ) !== 0 ) {
+			throw new \RuntimeException( sprintf( 'Curl error for "%1$s": "%2$s"', $url, curl_error( $ch ) ) );
+		}
+
+		if( ( $httpcode = curl_getinfo( $ch, CURLINFO_HTTP_CODE ) ) === false ) {
+			throw new \RuntimeException( sprintf( 'Curl getinfo failed for "%1$s": %2$s', $url, curl_error( $ch ) ) );
+		}
+
+		curl_close( $ch );
+
+		if( ( $result = json_decode( $response, true ) ) === null || !is_array( $result ) ) {
+			throw new \RuntimeException( sprintf( 'Invalid repsonse for "%1$s": %2$s', $url, $response ) );
+		}
+
+		return [$result, $httpcode];
+	}
+}
